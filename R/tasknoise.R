dfbe198c83abe7e79b75667fd8b92de9b0581e54
jdgryse <jasperdegryse@gmail.com>
2019-07-10 14:55:30 +0200

    solved issue with rescaling noise in active voxels

--- R/tasknoise.R
+++ R/tasknoise.R
@@ -13,7 +13,11 @@ function(act.image, sigma, type=c("gaussian","rician"), vee=1){
 		stop("The activation array has more than 4 dimensions")
 	}
 	
-
+  #Array indicating location of active voxels			
+  indvar <- apply(act.image, c(1,2,3), var) == 0
+  indmean <- apply(act.image, c(1,2,3), mean) == 0
+  indimage <-  array(1-(indvar*indmean), dim = dim)
+	
 	if(length(dim)==0){
 	    if(type=="gaussian"){
 		noise <-rnorm(length(act.image), 0, sigma)
@@ -23,14 +27,20 @@ function(act.image, sigma, type=c("gaussian","rician"), vee=1){
 	} else {
 	    if(type=="gaussian"){
 		noise <- array(rnorm(prod(dim), 0, sigma), dim=dim)
+
 	    } else {
 		noise <- array(rrice(prod(dim), vee, sigma), dim=dim)
 	    }
 	}
-	ix <- which(zapsmall(act.image)!=0)
-	noise[-ix] <- 0
 	
-	# Rescale noise
-	noise <- noise*sigma/sd(noise)
-	return(noise)
+  ix <- which(zapsmall(act.image)!=0)
+  noise[-ix] <- 0
+
+	
+  # Rescale noise
+  noise <- noise*sigma/sd(noise[indimage == 1])
+  return(noise)
+
 }
+
+
