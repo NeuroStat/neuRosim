eb1354da40633b996d1b923fc89f83a1881ed74c
HBossier <bossier.han@gmail.com>
2019-08-07 15:15:21 +0200

    Bug when dim equals c(1).
    For the function simTSrestingstate.R, the dim gets c(1). In this case,
    need to change dim to c(1,1,1) and then revert to one vector again.

--- R/physnoise.R
+++ R/physnoise.R
@@ -4,16 +4,25 @@ physnoise <-
     if(length(dim)>3){
       stop("Image space with more than three dimensions is not supported.")
     }
+    
+    # If only 1 voxel (e.g. for function simTSrestingstate.r), need to adapt dim
+    adapt_dim <- FALSE
+    if(sum(dim) == 1){
+      adapt_dim <- TRUE
+      dim <- c(dim, dim, dim)
+    }
+    
+    # Create time series
     HB <- 2*pi*freq.heart*TR
     RR <- 2*pi*freq.resp*TR
     t <- 1:nscan
     
     HRdrift <- sin(HB*t) + cos(RR*t)
-
+    
     # With Gaussian
     sigma.HR <- sd(HRdrift)
     HRweight <- sigma/sigma.HR
-    noise_gauss <- array(rnorm(prod(dim)*nscan, 0, 1), dim=c(dim, nscan))
+    noise_gauss <- array(rnorm(prod(dim)*nscan, 0, 1), dim = c(dim, nscan))
     noise_phys <- aperm(array(HRweight*HRdrift, dim = c(nscan, dim)), c(2,3,4,1))
     noise <- noise_gauss + noise_phys
     
@@ -28,7 +37,11 @@ physnoise <-
     
     # Rescale noise
     noise <- noise*sigma/sd(noise)
+    
+    # Switch to 1D if needed
+    if(adapt_dim){
+      noise <- c(noise)
+    }
     return(noise)
   }
-
 
